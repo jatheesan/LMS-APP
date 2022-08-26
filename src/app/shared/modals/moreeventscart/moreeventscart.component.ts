7cce2a1d31f9139f71b2ceef8c3f1557160db924
jatheesan <srijathee@gmail.com>
2022-08-26 09:46:33 +0530

    view leave details in show more modal

--- src/app/shared/modals/moreeventscart/moreeventscart.component.ts
+++ src/app/shared/modals/moreeventscart/moreeventscart.component.ts
@@ -1,14 +1,15 @@
-import { Component, Inject, OnInit, DoCheck } from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject, OnInit, DoCheck, HostListener } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import * as moment from 'moment';
 import { Moment } from 'moment';
 import { LeaveRequest } from 'src/app/models/leaverequest.model';
 import { Rowevent } from 'src/app/models/rowevent.model';
+import { ShowLeaveComponent } from '../leaves/show-leave/show-leave.component';
 
 @Component({
   selector: 'lms-moreeventscart',
   templateUrl: './moreeventscart.component.html',
-  styleUrls: ['./moreeventscart.component.scss']
+  styleUrls: ['../add-edit-modal-styles.scss']
 })
 export class MoreeventscartComponent implements OnInit {
 
@@ -16,12 +17,39 @@ export class MoreeventscartComponent implements OnInit {
   rowEvent: Rowevent[] = [];
   date!: string;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data : any) { }
+  constructor(
+    public dailog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public data : any,
+    public dialogRef : MatDialogRef<MoreeventscartComponent>,
+    ) { }
 
   ngOnInit(): void {
     this.leaveRequests = this.data.leaves;
     this.rowEvent = this.data.rowEvent;
     this.date = moment(this.data.date).format("DD-MM-YYYY");
+  }
+
+  // @HostListener('document:mouseover', ['$event'])
+  // mouseover(event: any){
+  //   if(event.target.matches('#event')){
+  //     let eventName = document.getElementById('event');
+  //     eventName!.style.color = "red";
+  //     console.log(eventName?.style);
+  //     //event.target.matches('#event').styles.color = "#fff";
+  //   }
+  // }
+
+  showevent(event : any){
+    this.dialogRef.close();
+    if(event){
+      const dialogRef = this.dailog.open(ShowLeaveComponent, {
+        width : '500px',
+        panelClass: 'custom-Leavebox',
+        data : {
+          leave :event,
+        }
+      });
+    }
   }
 
 
